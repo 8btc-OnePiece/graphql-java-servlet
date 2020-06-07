ff1df5c82dd7361df26ff4a34d1bbafc20462235
guodage <guodage003@gmail.com>
2020-06-07 18:15:30 +0800

    extend DefaultGraphQLErrorHandler, accept more client error.

--- graphql-java-kickstart/src/main/java/graphql/kickstart/execution/error/DefaultGraphQLErrorHandler.java
+++ graphql-java-kickstart/src/main/java/graphql/kickstart/execution/error/DefaultGraphQLErrorHandler.java
@@ -3,9 +3,11 @@ package graphql.kickstart.execution.error;
 import graphql.ExceptionWhileDataFetching;
 import graphql.GraphQLError;
 import graphql.execution.NonNullableFieldWasNullError;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.CompletionException;
+import java.util.stream.Collectors;
 
 /**
  * @author Andrew Potter
@@ -13,51 +15,56 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DefaultGraphQLErrorHandler implements GraphQLErrorHandler {
 
-  @Override
-  public List<GraphQLError> processErrors(List<GraphQLError> errors) {
-    final List<GraphQLError> clientErrors = filterGraphQLErrors(errors);
-    if (clientErrors.size() < errors.size()) {
+    @Override
+    public List<GraphQLError> processErrors(List<GraphQLError> errors) {
+        final List<GraphQLError> clientErrors = filterGraphQLErrors(errors);
+        if (clientErrors.size() < errors.size()) {
 
-      // Some errors were filtered out to hide implementation - put a generic error in place.
-      clientErrors.add(new GenericGraphQLError("Internal Server Error(s) while executing query"));
+            // Some errors were filtered out to hide implementation - put a generic error in place.
+            clientErrors.add(new GenericGraphQLError("Internal Server Error(s) while executing query"));
 
-      errors.stream()
-          .filter(error -> !isClientError(error))
-          .forEach(this::logError);
+            errors.stream()
+                    .filter(error -> !isClientError(error))
+                    .forEach(this::logError);
+        }
+
+        return clientErrors;
     }
 
-    return clientErrors;
-  }
-
-  protected void logError(GraphQLError error) {
-    if (error instanceof Throwable) {
-      log.error("Error executing query!", (Throwable) error);
-    } else if (error instanceof ExceptionWhileDataFetching) {
-      log.error("Error executing query {}", error.getMessage(), ((ExceptionWhileDataFetching) error).getException());
-    } else {
-      log.error("Error executing query ({}): {}", error.getClass().getSimpleName(), error.getMessage());
+    protected void logError(GraphQLError error) {
+        if (error instanceof Throwable) {
+            log.error("Error executing query!", (Throwable) error);
+        } else if (error instanceof ExceptionWhileDataFetching) {
+            log.error("Error executing query {}", error.getMessage(), ((ExceptionWhileDataFetching) error).getException());
+        } else {
+            log.error("Error executing query ({}): {}", error.getClass().getSimpleName(), error.getMessage());
+        }
     }
-  }
 
-  protected List<GraphQLError> filterGraphQLErrors(List<GraphQLError> errors) {
-    return errors.stream()
-        .filter(this::isClientError)
-        .map(this::replaceNonNullableFieldWasNullError)
-        .collect(Collectors.toList());
-  }
-
-  protected boolean isClientError(GraphQLError error) {
-    if (error instanceof ExceptionWhileDataFetching) {
-      return ((ExceptionWhileDataFetching) error).getException() instanceof GraphQLError;
+    protected List<GraphQLError> filterGraphQLErrors(List<GraphQLError> errors) {
+        return errors.stream()
+                .filter(this::isClientError)
+                .map(this::replaceNonNullableFieldWasNullError)
+                .collect(Collectors.toList());
     }
-    return true;
-  }
 
-  private GraphQLError replaceNonNullableFieldWasNullError(GraphQLError error) {
-    if (error instanceof NonNullableFieldWasNullError) {
-      return new RenderableNonNullableFieldWasNullError((NonNullableFieldWasNullError) error);
-    } else {
-      return error;
+    protected boolean isClientError(GraphQLError error) {
+        if (error instanceof ExceptionWhileDataFetching) {
+            Throwable throwable = ((ExceptionWhileDataFetching) error).getException();
+            return throwable instanceof GraphQLError || throwable.getCause() instanceof GraphQLError;
+        }
+        return true;
     }
-  }
+
+    private GraphQLError replaceNonNullableFieldWasNullError(GraphQLError error) {
+        if (error instanceof NonNullableFieldWasNullError) {
+            return new RenderableNonNullableFieldWasNullError((NonNullableFieldWasNullError) error);
+        } else if (error instanceof ExceptionWhileDataFetching) {
+            Throwable throwable = ((ExceptionWhileDataFetching) error).getException();
+            if (throwable instanceof CompletionException && throwable.getCause() instanceof GraphQLError) {
+                return (GraphQLError) throwable.getCause();
+            }
+        }
+        return error;
+    }
 }
